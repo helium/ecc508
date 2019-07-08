25d3917a899dc718e6ae0d8cd088e4933a231fc6
Marc Nijdam <marc@helium.com>
2019-07-08 23:44:26 +0100

    Read commands seem to work.. trying to parse slots

--- src/ecc508.erl
+++ src/ecc508.erl
@@ -3,23 +3,27 @@
 -include_lib("public_key/include/public_key.hrl").
 
 %% API exports
--export([wake/1,
+-export([start_link/0,
+         wake/1,
          genkey/4,
          nonce/3,
          digest_init/2, digest_update/3, digest_finalize/3,
          random/1, random/2,
          sign/3, verify/4,
          pause/2,
-         write/3
+         read/3,
+         write/3,
+         slot_config/2
         ]).
 %% supporting functions
 -export([encode_address/1,
          execute/2,
-         command_spec/1, command/4
+         command_spec/1, command/4,
+         to_hex/1
         ]).
 
 -type address() :: {otp, Block::non_neg_integer(), Offset::non_neg_integer()} |
-                   {config, Block::non_neg_integer(), Offset::non_neg_integer()} |
+                   {config, Block::0..3, Offset::non_neg_integer()} |
                    {data, Slot::non_neg_integer(), Block::non_neg_integer()} |
                    {data, Slot::non_neg_integer(), Block::non_neg_integer(), Offset::non_neg_integer()}.
 -export_type([address/0]).
@@ -42,12 +46,62 @@
                   data = <<>> :: binary()
                  }).
 
+
+start_link() ->
+    i2c:start_link("i2c-1", 16#60, 155).
+
 %%====================================================================
 %% API functions
 %%====================================================================
 
 wake(Pid) ->
     execute(Pid, command(wake)).
+
+
+-record(slot_config, {
+                      write_config :: <<_:4>>,
+                      write_key :: <<_:4>>,
+                      is_secret :: boolean(),
+                      encrypt_read :: boolean(),
+                      limited_use :: boolean(),
+                      no_mac :: boolean(),
+                      read_key :: <<_:3>>
+                     }).
+
+slot_config(P, Slot) when Slot >= 0, Slot =< 15 ->
+    {Block, Offset} = case Slot =< 5 of
+                          true ->
+                              {0, (20 + Slot * 2) bsr 2};
+                          false ->
+                              {1, ((Slot - 5) * 2) bsr 2}
+                      end,
+    case read(P, 4, {config, Block, Offset}) of
+        {ok, <<S0:16/bitstring, S1:16/bitstring>>} ->
+            case Slot rem 2 of
+                0 -> {ok, parse_slot_config(S0)};
+                1 -> {ok, parse_slot_config(S1)}
+            end;
+        {error, Error} ->
+            {error, Error}
+    end.
+
+
+parse_slot_config(<<WriteConfig:4,
+                    WriteKey:4,
+                    IsSecret:1,
+                    EncryptRead:1,
+                    LimitedUse:1,
+                    NoMac:1,
+                    ReadKey:4>> = V) ->
+    io:format("PARSING ~p, HEX ~p~n", [V, to_hex(V)]),
+    #slot_config{write_config=WriteConfig,
+                 write_key=WriteKey,
+                 is_secret=bit_to_bool(IsSecret),
+                 encrypt_read=bit_to_bool(EncryptRead),
+                 limited_use=bit_to_bool(LimitedUse),
+                 no_mac=bit_to_bool(NoMac),
+                 read_key=ReadKey}.
+
 
 genkey(Pid, private, KeyId, Opts=#{from_scratch := _FromScratch,
                                    should_store := _ShouldStore}) ->
@@ -158,6 +212,9 @@ sign(Pid, external, KeyId) ->
 verify(Pid, external, Signature, ECPubKey) ->
     execute(Pid, command({verify, {external, Signature, ECPubKey}})).
 
+-spec read(I2C::pid(), Size::4 | 32, Address::address()) -> {ok, binary()} | {error, term()}.
+read(Pid, Size, Address) ->
+    execute(Pid, command({read, Size, Address})).
 
 -spec write(I2C::pid(), Address::address(), Data::<<_:32>> | <<_:256>>) -> ok | {error, term()}.
 write(Pid, Address, <<Data/binary>>) ->
@@ -192,33 +249,49 @@ command_spec(N=verify)      -> #command_spec{name=N, opcode=16#45, timing_typ=38
 command_spec(N=write)       -> #command_spec{name=N, opcode=16#12, timing_typ=7,  timing_max=26};
 command_spec(N=wake)        -> #command_spec{name=N, opcode=16#00, timing_typ=0,  timing_max=1}. %% not in spec
 
+
+-spec encode_address_zone(Address::address()) -> 0..2.
+encode_address_zone({config, _, _}) -> 16#00;
+encode_address_zone({otp, _, _}) -> 16#01;
+encode_address_zone({data, _, _}) -> 16#02;
+encode_address_zone({data, _, _, _}) -> 16#02;
+encode_address_zone(_) ->  throw(ecc_zone_invalid).
+
 %% @private
 %% encode_address/config should not be used to write addresses at
 %% offsets 0-16, and shouldn't be used with the Write command at
 %% offsets 84-87, as per 508 Datasheet Section 9.1.4, page 59.
+%%
+%% Note that the address is in Param2 for all commands and is encoded
+%% with LSB first, i.e. reversed from the documentation.
+%%
 %% @end
 -spec encode_address(Address::address()) -> binary ().
-encode_address({Zone, Location}) ->
-    encode_address({Zone, trunc(Location div 32), Location rem 32});
-encode_address({otp, Block, Offset}) ->
-    <<0:8, 0:4, Block:1, Offset:3>>;
-encode_address({config, Block, Offset}) ->
-    <<0:8, 0:3, Block:2, Offset:3>>;
+encode_address({otp, Block, Offset}) when Block >= 0, Block =< 1->
+    <<0:4, Block:1, Offset:3, 0:8>>;
+encode_address({config, Block, Offset}) when Block >= 0, Block =< 3, Offset >= 0, Offset =< 7->
+    <<0:3, Block:2, Offset:3, 0:8>>;
 encode_address({data, Slot, Block}) ->
     encode_address({data, Slot, Block, 0});
 encode_address({data, Slot, Block, Offset}) when Slot < 8->
-    <<0:7, Block:1, 0:1, Slot:4, Offset:3>>;
+    <<0:1, Slot:4, Offset:3, 0:7, Block:1>>;
 encode_address({data, Slot, Block, Offset}) when Slot == 8 ->
-    <<0:4, Block:4, 0:1, Slot:4, Offset:3>>;
+    <<0:1, Slot:4, Offset:3, 0:4, Block:4>>;
 encode_address({data, Slot, Block, Offset}) when Slot > 8 ->
-    <<0:6, Block:2, 0:1, Slot:4, Offset:3>>.
+    <<0:1, Slot:4, Offset:3, 0:6, Block:2>>.
 
-bool_to_bit(Val) ->
-    case Val of
-        true -> 1;
-        false -> 0;
-        _ -> throw({not_boolean, Val})
-    end.
+
+encode_read_write_size(4) -> 0;
+encode_read_write_size(32) -> 1;
+encode_read_write_size(Val) ->  throw({ecc_size_invalid, Val}).
+
+bool_to_bit(true) -> 1;
+bool_to_bit(false) -> 0;
+bool_to_bit(Val) ->throw({not_boolean, Val}).
+
+bit_to_bool(1) -> true;
+bit_to_bool(0) -> false;
+bit_to_bool(V) -> throw({not_boolean_bit, V}).
 
 -spec command(Cmd::atom(), Param1::<<_:8>>, Param2::<<_:16>>, Data::binary()) -> #command{}.
 command(Type, Param1, Param2, Data) ->
@@ -252,35 +325,33 @@ command({random, SeedMode}) ->
 command({digest, {init, sha}}) ->
     command(sha, <<0:8>>, <<0:16>>, <<>>);
 command({digest, {init, {hmac, Slot}}}) ->
-    command(sha, <<16#04>>, <<Slot:16>>, <<>>);
+    command(sha, <<16#04>>, <<Slot:16/integer-unsigned-big>>, <<>>);
 command({digest, {update, Data}}) when byte_size(Data) =< 64->
-    command(sha, <<16#01>>, <<(byte_size(Data)):16>>, Data);
+    command(sha, <<16#01>>, <<(byte_size(Data)):16/integer-unsigned-big>>, Data);
 command({digest, {public, Slot}}) ->
-    command(sha, <<16#03>>, <<Slot:16>>, <<>>);
+    command(sha, <<16#03>>, <<Slot:16/integer-unsigned-big>>, <<>>);
 command({digest, {finalize, {sha, Data}}}) ->
-    command(sha, <<16#02>>, <<(byte_size(Data)):16>>, Data);
+    command(sha, <<16#02>>, <<(byte_size(Data)):16/integer-unsigned-big>>, Data);
 command({digest, {finalize, {hmac, Data}}}) ->
-    command(sha, <<16#05>>, <<(byte_size(Data)):16>>, Data);
+    command(sha, <<16#05>>, <<(byte_size(Data)):16/integer-unsigned-big>>, Data);
 command({sign, {external, KeyId}}) ->
-    command(sign, <<16#80>>, <<KeyId:16>>, <<>>);
+    command(sign, <<16#80>>, <<KeyId:16/integer-unsigned-big>>, <<>>);
 command({verify, {external, Signature, _ECPubKey={#'ECPoint'{point=PubPoint}, _}}}) ->
     << _:8, X:32/binary, Y:32/binary>> = PubPoint,
     SignatureLen = byte_size(Signature),
     {R, S} = split_binary(Signature, SignatureLen div 2),
     Data = <<R/binary, S/binary, X/binary, Y/binary>>,
-    command(verify, <<16#02>>, <<16#00, 16#03>>, Data);
+    command(verify, <<16#02>>, <<16#03, 16#00>>, Data);
+command({read, Size, Address}) ->
+    Param1 = <<(encode_read_write_size(Size)):1, 0:5, (encode_address_zone(Address)):2>>,
+    command(read, Param1, encode_address(Address), <<>>);
 command({write, Address, Data}) ->
-    ByteSizeBit = fun(D) when byte_size(D) == 4-> 0;
-                     (D) when byte_size(D) == 32 -> 1;
-                     (_) -> throw(ecc_write_size_invalid)
-                  end,
-    SectionValue = fun(config) -> 16#00;
-                      (otp) -> 16#01;
-                      (data) -> 16#02;
-                      (_) ->  throw(ecc_write_section_invalid)
-                   end,
-    Param1 = <<(ByteSizeBit(Data)):1, 0:1, 0:4, (SectionValue(element(1, Address))):2>>,
+    Param1 = <<(encode_read_write_size(byte_size(Data))):1, 0:5, (encode_address_zone(Address)):2>>,
     command(write, Param1, encode_address(Address), Data).
+
+
+to_hex(Bin) ->
+    << <<Y>> ||<<X:4>> <= Bin, Y <- integer_to_list(X,16)>>.
 
 -spec wait_for_response(I2C::pid(), {WaitExtra::boolean(), WaitStartTime::pos_integer()}, Cmd::#command{})
                        -> {error, term()} | {ok, pos_integer()}.
